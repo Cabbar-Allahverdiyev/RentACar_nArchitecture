dff056f4dfac4e0a402a48923bb814d4adeca2ed
Cabbar Allahverdiyev <cabbar.allahverdiyev@gmail.com>
2022-09-01 00:41:10 +0400

    GetByIdQuery added

--- src/demoProjects/rentACar/WebAPI/Controllers/BrandsController.cs
+++ src/demoProjects/rentACar/WebAPI/Controllers/BrandsController.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Features.Brands.Commands.Create;
 using Application.Features.Brands.Dtos;
 using Application.Features.Brands.Models;
+using Application.Features.Brands.Queries.GetByIdBrand;
 using Application.Features.Brands.Queries.GetListBrand;
 using Core.Application.Requests;
 using Microsoft.AspNetCore.Mvc;
@@ -24,6 +25,14 @@ namespace WebAPI.Controllers
             GetListBrandQuery getListBrandQuery = new() { PageRequest = pageRequest };
             BrandListModel result = await Mediator.Send(getListBrandQuery);
             return Ok(result);
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<IActionResult> GetById([FromRoute] GetByIdBrandQuery getByIdBrandQuery)
+        {
+            BrandGetByIdDto  getByIdBrandDto = await Mediator.Send(getByIdBrandQuery);
+           
+            return Ok(getByIdBrandDto);
         }
     }
 }
